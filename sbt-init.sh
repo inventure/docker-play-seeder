e3f96369c07cd4fbd2e5c082070b38e2a9361ebb
Ian Nowland <ian.nowland@tala.co>
2022-08-12 13:02:30 -0400

    Also updating sbt-init.sh

--- sbt-init.sh
+++ sbt-init.sh
@@ -5,7 +5,7 @@ source $HOME/.sdkman/bin/sdkman-init.sh
 # Install Java and SBT - Reference: https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
 # By default, it installs the latest version of Java 8 from OpenJDK (jdk.java.net)
 sdk install java 8.0.342-amzn
-sdk install sbt 1.4.9
+sdk install sbt 1.7.1
 
 # Create a symlink to /usr/bin so they can be used in plain sh
 ln -s $HOME/.sdkman/candidates/sbt/current/bin/sbt /usr/bin/sbt
