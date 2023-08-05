3b4ab6d2508f3448447ffbbbd8dbdf53f633ab38
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2023-08-05 11:08:19 -0700

    Upgrade SBT to 1.9.3

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 // Fix Scala XML compatibility issue when using SBT 1.8.x https://eed3si9n.com/sbt-1.8.0
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.19")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.20")
--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val javaVersion = "8.0.372-amzn"
+  val javaVersion = "8.0.382-amzn"
   val playVersion = "2.8.20"
   val playJsonVersion = "2.9.4"
   val playSlickVersion = "3.0.4"
   val scalaVersion = "2.12.18"
-  val sbtVersion = "1.9.2" //make sure to also bump build.properties
+  val sbtVersion = "1.9.3" //make sure to also bump build.properties
 }
