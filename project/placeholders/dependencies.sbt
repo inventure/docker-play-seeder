62aa4d118d004a084a3da58d957baa30d6cdd03c
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2023-10-23 11:46:42 -0700

    Drop non-core SBT modules

--- project/placeholders/dependencies.sbt
+++ project/placeholders/dependencies.sbt
@@ -2,10 +2,6 @@
 scalaVersion := "[scala_version]"
 
 libraryDependencies := Seq(
-  ws,
-  guice,
-  specs2 % Test,[caffeine]
-  "com.typesafe.play" %% "play-json" % "[play_json_version]",
   "com.typesafe.play" %% "play-slick" % "[play_slick_version]",
   "com.typesafe.play" %% "play-slick-evolutions" % "[play_slick_version]"
 )
--- project/versions.scala
+++ project/versions.scala
@@ -2,7 +2,6 @@ object versions {
   val baseImage = "debian:bullseye-20231009-slim"
   val javaVersion = "8.0.392-amzn"
   val playVersion = "2.8.20"
-  val playJsonVersion = "2.9.4"
   val playSlickVersion = "3.0.4"
   val scalaVersion = "2.12.18"
   val sbtVersion = "1.9.7" //make sure to also bump build.properties and sbt-init.sh
