f438d7fad2323db87d90a77429fe7e5c5af939e4
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2023-10-23 09:07:44 -0700

    Set default Java version to 8.0.392-amzn

--- project/versions.scala
+++ project/versions.scala
@@ -1,6 +1,6 @@
 object versions {
   val baseImage = "debian:bullseye-20231009-slim"
-  val javaVersion = "8.0.382-amzn"
+  val javaVersion = "8.0.392-amzn"
   val playVersion = "2.8.20"
   val playJsonVersion = "2.9.4"
   val playSlickVersion = "3.0.4"
