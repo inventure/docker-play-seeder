1b994e1f7a9437a3661d982f4640b9bf04da451a
Ian Nowland <ian.nowland@tala.co>
2024-04-03 13:11:06 -0400

    Bumping patch versions for base image, java version, and scala version

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,5 @@
 // Fix Scala XML compatibility issue when using SBT 1.8.x https://eed3si9n.com/sbt-1.8.0
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
+//Make sure this matches the version of play in versions.scala
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.21")
--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20240211-slim"
-  val javaVersion = "11.0.21-amzn"
-  val playVersion = "2.8.21"
+  val baseImage = "debian:bullseye-20240311-slim"
+  val javaVersion = "11.0.22-amzn"
+  val playVersion = "2.8.21" //make sure the project/plugins.sbt matches this number
   val playSlickVersion = "5.1.0"
-  val scalaVersion = "2.13.12"
+  val scalaVersion = "2.13.13"
   val sbtVersion = "1.9.9" //make sure to also bump build.properties and sbt-init.sh
 }
