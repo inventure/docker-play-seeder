a966196dade4a88c39b1e0839c0dae3a34457977
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2024-07-08 09:57:57 -0700

    Update play-slick to 5.3.1

--- project/versions.scala
+++ project/versions.scala
@@ -2,7 +2,7 @@ object versions {
   val baseImage = "debian:bullseye-20240701-slim"
   val javaVersion = "11.0.23-amzn"
   val playVersion = "2.9.4"
-  val playSlickVersion = "5.3.0"
+  val playSlickVersion = "5.3.1"
   val scalaVersion = "2.13.14"
   val sbtVersion = "1.10.1" //make sure to also bump build.properties and sbt-init.sh
 }
