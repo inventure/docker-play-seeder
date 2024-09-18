cb0a10b6afb9aadfa13df4ebddf45a2a3ef7de1b
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2024-09-17 18:43:58 -0700

    Set default base image to bullseye-20240904-slim

--- project/versions.scala
+++ project/versions.scala
@@ -1,5 +1,5 @@
 object versions {
-  val baseImage = "debian:bullseye-20240722-slim"
+  val baseImage = "debian:bullseye-20240904-slim"
   val javaVersion = "11.0.24-amzn"
   val playVersion = "2.9.5"
   val playSlickVersion = "5.3.1"
