2d13a925f933052cacfbd8db5273e13f281a1d9b
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2024-09-25 17:29:31 -0700

    Update default Scala version to 2.13.15

--- project/versions.scala
+++ project/versions.scala
@@ -3,6 +3,6 @@ object versions {
   val javaVersion = "11.0.24-amzn"
   val playVersion = "2.9.5"
   val playSlickVersion = "5.3.1"
-  val scalaVersion = "2.13.14"
+  val scalaVersion = "2.13.15"
   val sbtVersion = "1.10.2" //make sure to also bump build.properties and sbt-init.sh
 }
