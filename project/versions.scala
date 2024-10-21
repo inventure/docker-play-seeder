553466810fa1b9285b492c2b51206310c817ec8d
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2024-10-21 09:00:03 -0700

    Update default java version to 11.0.25-amzn

--- project/versions.scala
+++ project/versions.scala
@@ -1,6 +1,6 @@
 object versions {
   val baseImage = "debian:bullseye-20240926-slim"
-  val javaVersion = "11.0.24-amzn"
+  val javaVersion = "11.0.25-amzn"
   val playVersion = "2.9.5"
   val playSlickVersion = "5.3.1"
   val scalaVersion = "2.13.15"
