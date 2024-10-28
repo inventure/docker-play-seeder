baae1a59fb1b9d0df0b8adff817e2ff5d1314693
Ian Nowland <ian.nowland@tala.co>
2024-10-28 10:40:46 -0400

    Updating sbt 1.10.3 -> 1.10.4

--- sbt-init.sh
+++ sbt-init.sh
@@ -5,7 +5,7 @@ source $SDKMAN_DIR/bin/sdkman-init.sh
 # Install Java and SBT - Reference: https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
 sdk update
 sdk install java 11.0.25-amzn
-sdk install sbt 1.10.3
+sdk install sbt 1.10.4
 
 # Create a symlink to /usr/bin so they can be used in plain sh
 ln -s $SDKMAN_DIR/candidates/sbt/current/bin/sbt /usr/bin/sbt
