a551a8e114f8d66d6c111269557221dd5fdc472f
Ian Nowland <ian.nowland@tala.co>
2025-01-02 16:53:14 -0500

    Updating sbt and docker image

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20241111-slim"
+  val baseImage = "debian:bullseye-20241223-slim"
   val javaVersion = "11.0.25-amzn"
   val playVersion = "2.9.6"
   val playSlickVersion = "5.3.1"
   val scalaVersion = "2.13.15"
-  val sbtVersion = "1.10.6" //make sure to also bump build.properties and sbt-init.sh
+  val sbtVersion = "1.10.7" //make sure to also bump build.properties and sbt-init.sh
 }
