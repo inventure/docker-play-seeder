7b5ba07e9401fdb3fa67c223de5b0d3a40bf078c
Ian Nowland <ian.nowland@tala.co>
2025-01-10 10:20:23 -0500

    Bumping java 11.0.25 -> 21.0.5

--- sbt-init.sh
+++ sbt-init.sh
@@ -4,7 +4,7 @@ source $SDKMAN_DIR/bin/sdkman-init.sh
 
 # Install Java and SBT - Reference: https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
 sdk update
-sdk install java 11.0.25-amzn
+sdk install java 21.0.5-amzn
 sdk install sbt 1.10.7
 
 # Create a symlink to /usr/bin so they can be used in plain sh
