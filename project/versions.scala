a63c0edd04cb11a8686333a0dbc1f592988e2082
Ian Nowland <ian.nowland@tala.co>
2025-01-29 14:46:29 -0500

    Updating java and scala

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20241223-slim"
-  val javaVersion = "21.0.5-amzn"
+  val baseImage = "debian:bullseye-20250113-slim"
+  val javaVersion = "21.0.6-amzn"
   val playVersion = "2.9.6"
   val playSlickVersion = "5.3.1"
-  val scalaVersion = "2.13.15"
+  val scalaVersion = "2.13.16"
   val sbtVersion = "1.10.7" //make sure to also bump build.properties and sbt-init.sh
 }
