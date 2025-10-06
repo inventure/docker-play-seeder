11530e7b3ab4045eb868c39dc5c41d57618e08db
Ian Nowland <ian.nowland@tala.co>
2025-10-06 09:28:51 -0700

    Upgrading sbt -> 1.11.7 and debian -> 20250929

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20250908-slim"
+  val baseImage = "debian:bullseye-20250929-slim"
   val javaVersion = "21.0.8-amzn"
   val playVersion = "2.9.9"
   val playSlickVersion = "5.4.0"
   val scalaVersion = "2.13.16"
-  val sbtVersion = "1.11.6"
+  val sbtVersion = "1.11.7"
 }
