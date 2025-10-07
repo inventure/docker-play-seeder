51ffb0fdf6c5c6cfe15cebb20e624aa5f039c09c
Ian Nowland <ian.nowland@tala.co>
2025-10-07 08:00:00 -0700

    Also updating Scala

--- project/versions.scala
+++ project/versions.scala
@@ -3,6 +3,6 @@ object versions {
   val javaVersion = "21.0.8-amzn"
   val playVersion = "2.9.9"
   val playSlickVersion = "5.4.0"
-  val scalaVersion = "2.13.16"
+  val scalaVersion = "2.13.17"
   val sbtVersion = "1.11.7"
 }
