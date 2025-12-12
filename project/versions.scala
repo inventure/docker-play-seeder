1e5d5c0779d522cd29b3cc97b7273149994e1e61
Ian Nowland <ian.nowland@tala.co>
2025-12-12 17:30:10 -0500

    Updating scala ->2.13.17, java -> 21.0.9, debian -> 20251208

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20250929-slim"
-  val javaVersion = "21.0.8-amzn"
+  val baseImage = "debian:bullseye-20251208-slim"
+  val javaVersion = "21.0.9-amzn"
   val playVersion = "2.9.9"
   val playSlickVersion = "5.4.0"
-  val scalaVersion = "2.13.17"
+  val scalaVersion = "2.13.18"
   val sbtVersion = "1.11.7"
 }
--- sbt-init.sh
+++ sbt-init.sh
@@ -4,7 +4,7 @@ source $SDKMAN_DIR/bin/sdkman-init.sh
 
 # Install Java and SBT - Reference: https://www.scala-sbt.org/1.x/docs/Installing-sbt-on-Linux.html
 sdk update
-sdk install java 21.0.8-amzn
+sdk install java 21.0.9-amzn
 sdk install sbt 1.11.7
 
 # Create a symlink to /usr/bin so they can be used in plain sh
