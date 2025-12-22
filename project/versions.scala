35cedca9fc2a786d6d22ffc26739e2f17417b9cc
Ian Nowland <ian.nowland@tala.co>
2025-12-22 10:31:47 -0500

    Updating play version

--- project/versions.scala
+++ project/versions.scala
@@ -1,7 +1,7 @@
 object versions {
   val baseImage = "debian:bullseye-20251208-slim"
   val javaVersion = "21.0.9-amzn"
-  val playVersion = "2.9.9"
+  val playVersion = "2.9.10"
   val playSlickVersion = "5.4.0"
   val scalaVersion = "2.13.18"
   val sbtVersion = "1.11.7"
