25e6da39282c301c7e45d6460b5c3aead6bed01e
Ian Nowland <ian.nowland@tala.co>
2026-01-07 13:01:17 -0500

    Bumping versions and upgrading to play

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20251208-slim"
+  val baseImage = "debian:bullseye-20251229-slim"
   val javaVersion = "21.0.9-amzn"
-  val playVersion = "2.9.10"
-  val playSlickVersion = "5.4.0"
+  val playVersion = "3.0.6"
+  val playSlickVersion = "6.1.0"
   val scalaVersion = "2.13.18"
-  val sbtVersion = "1.11.7"
+  val sbtVersion = "1.12.0"
 }
