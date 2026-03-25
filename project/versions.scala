c75003710633c2e8baeccf88baa15f8a21e9ace3
Ian Nowland <ian.nowland@tala.co>
2026-03-25 10:27:33 -0400

    Updating debian -> 20260316 and sbt -> 1.12.8

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:bullseye-20260223-slim"
+  val baseImage = "debian:bullseye-20260316-slim"
   val javaVersion = "21.0.10-amzn"
   val playVersion = "3.0.10"
   val playSlickVersion = "6.2.0"
   val scalaVersion = "2.13.18"
-  val sbtVersion = "1.12.5"
+  val sbtVersion = "1.12.8"
 }
