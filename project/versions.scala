66937256abf6dc43209093f89b141d7fcbe012f6
Lawrence Asrikin <5559568+LawrenceAsrikin@users.noreply.github.com>
2026-05-24 17:37:50 -0700

    Update base Debian and PLay versions

--- project/versions.scala
+++ project/versions.scala
@@ -1,7 +1,7 @@
 object versions {
-  val baseImage = "debian:bullseye-20260421-slim"
+  val baseImage = "debian:bullseye-20260518-slim"
   val javaVersion = "21.0.11-amzn"
-  val playVersion = "3.0.10"
+  val playVersion = "3.0.11"
   val playSlickVersion = "6.2.0"
   val scalaVersion = "2.13.18"
   val sbtVersion = "1.12.11"
