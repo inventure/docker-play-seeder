5251ddec2536698775923241b0cd583171bb6e6f
Ian Nowland <ian.nowland@tala.co>
2026-07-13 11:21:34 -0400

    Updating trixie -> 20260623 and sbt -> 1.12.13

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:trixie-20260610-slim"
+  val baseImage = "debian:trixie-20260623-slim"
   val javaVersion = "21.0.11-amzn"
   val playVersion = "3.0.11"
   val playSlickVersion = "6.2.0"
   val scalaVersion = "2.13.18"
-  val sbtVersion = "1.12.12"
+  val sbtVersion = "1.12.13"
 }
