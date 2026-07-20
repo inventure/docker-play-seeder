52f668717dffa8f44cdfba91c39f974ee1e4a821
Ian Nowland <ian.nowland@tala.co>
2026-07-20 10:08:04 -0400

    Updating base image and sbt version -> 1.12.14

--- project/versions.scala
+++ project/versions.scala
@@ -1,8 +1,8 @@
 object versions {
-  val baseImage = "debian:trixie-20260623-slim"
+  val baseImage = "debian:trixie-20260713-slim"
   val javaVersion = "21.0.11-amzn"
   val playVersion = "3.0.11"
   val playSlickVersion = "6.2.0"
   val scalaVersion = "2.13.18"
-  val sbtVersion = "1.12.13"
+  val sbtVersion = "1.12.14"
 }
